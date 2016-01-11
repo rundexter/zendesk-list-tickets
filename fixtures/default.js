a2b5e9b4efb159ad53caee647a52186fab2a48d6
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-11 13:04:29 +0200

    add DEXTER module

--- fixtures/default.js
+++ fixtures/default.js
@@ -63,7 +63,8 @@ module.exports = _.merge({
              */
             input: {
                 //Replace VAR1 with the name of an expected input, and add more inputs as needed.
-                VAR1: 'foo'
+                subject: 'My printer is on fire!',
+                comment: 'The smoke is very colorful.'
             }
         }
     }
--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+var util = require('./util.js'),
+    zendesk = require('node-zendesk');
+
+var pickInputs = {
+        'subject': { key: 'ticket.subject', validate: { req: true }},
+        'comment': 'ticket.comment.body'
+    },
+    pickOutputs = {
+        id: 'id',
+        requester_id: 'requester_id',
+        assignee_id: 'assignee_id',
+        collaborator_ids: 'collaborator_ids',
+        created_at: 'created_at',
+        subject: 'subject',
+        description: 'description',
+        status: 'status'
+    };
+
 module.exports = {
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +25,29 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var inputs = util.pickInputs(step, pickInputs),
+            validateErrors = util.checkValidateErrors(inputs, pickInputs),
+            token = dexter.environment('zendesk_token'),
+            username = dexter.environment('zendesk_username'),
+            subdomain = dexter.environment('zendesk_subdomain');
+
+        if (!token || !username || !subdomain)
+            return this.fail('A [zendesk_token,zendesk_username,zendesk_subdomain] environment variable is required for this module');
+
+        if (validateErrors)
+            return this.fail(validateErrors);
+        
+        var client = zendesk.createClient({
+            username:  username,
+            token:     token,
+            remoteUri: 'https://' + subdomain + '.zendesk.com/api/v2'
+        });
+
+        client.tickets.create(inputs, function (err, req, result) {
+            if (err)
+                this.fail(err);
+            else
+                this.complete(util.pickOutputs(result, pickOutputs));
+        }.bind(this));
     }
 };
